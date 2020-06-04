6527433c64aa0125cd6963f521e1173e3d84bb38
Azura Apple <dragonminner867@outlook.com>
2020-06-04 12:25:22 +0200

    Update terms.js

--- commands/Core/terms.js
+++ commands/Core/terms.js
@@ -18,7 +18,7 @@ class Terms extends Command {
     const embed = new RichEmbed()
       .setColor("RANDOM")
       .setTitle("__**Celestia Bot - Terms of Service**__")
-      .setDescription('\nByleth has access to the End User Data through the Discord API, but Celestia does not collect, use and/or disclose End User Data except (a) as necessary to exercise your rights under this Agreement, (b) in accordance with Discord’s Privacy Policy.' +
+      .setDescription('\nCelestia has access to the End User Data through the Discord API, but Celestia does not collect, use and/or disclose End User Data except (a) as necessary to exercise your rights under this Agreement, (b) in accordance with Discord’s Privacy Policy.' +
       '\n\nWe will never sell, license or otherwise commercialize any End User Data. Neither will we ever use End User Data to target End Users for marketing or advertising purposes. We will never even disclose any End User Data to any ad network, data broker or other advertising or monetization related service.' +
       '\n\nEnd User Data will be retained only as necessary to provide the defined functionality of the Application and nothing more.' +
       '\n\nWe ensure that all End User Data is stored using reasonable security measures and we take reasonable steps to secure End User Data.' +
