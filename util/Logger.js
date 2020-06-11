e9edcf68feec9a050f5b7469aad9afb7fe26f811
Azura Apple <dragonminner867@outlook.com>
2020-06-11 16:20:56 +0200

    Fix warn logger

--- util/Logger.js
+++ util/Logger.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2020 Azura Apple. All rights reserved. MIT license.
 
+// Logger class for unique and advanced logging
+
 const chalk = require("chalk");
 const moment = require("moment");
 
@@ -7,28 +9,28 @@ class Logger {
   static log(content, type = "log") {
     const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
     switch (type) {
-      case "log": {
+      case "log": { 
         return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
       }
-      case "warn": {
+      case "warn": { // Warn logger, usually runs if the eval command is used, as example
         return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
       }
-      case "info": {
+      case "info": { // Info logger, usually runs when the bot starts or shuts down
         return console.log(`${timestamp} ${chalk.cyan(type.toUpperCase())} ${content} `);
       }
-      case "error": {
+      case "error": { // Error logger, for logging advanced error messages
         return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
       }
-      case "debug": {
+      case "debug": { 
         return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
       }
-      case "cmd": {
+      case "cmd": { // When a command is run by someone, it'll log it
         return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
       }
-      case "panel": {
+      case "panel": { 
         return console.log(`${timestamp} ${chalk.black.bgCyan(type.toUpperCase())} ${content}`);
       }
-      case "ready": {
+      case "ready": { // Runs when the bot starts up. To this counts loading events and loading commands
         return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
       } 
       default: throw new TypeError("Logger type must be one of either log, warn, info, error, debug, cmd, panel or ready.");
