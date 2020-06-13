5e327e858281588840f5fce390c090c1449a69f7
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:05:49 +0200

    Several bug fixes

--- commands/Fun/bonzi.js
+++ commands/Fun/bonzi.js
@@ -16,35 +16,35 @@ class Bonzi extends Command {
   }
 
   async run(message, args, level, settings) {
-        let text = message.content.split(/\s+/g).slice(1).join(" ");
+      let text = args.join(" ");
 
-        if (!text) {
-            return message.react('🚫'), message.reply('Command Usage: `bonzi <Text>`');
-        }
+      if (!text) {
+          return message.react('🚫'), message.reply('Command Usage: `bonzi <Text>`');
+      }
 
-        await message.channel.startTyping()
+      await message.channel.startTyping()
 
-        let bonzi = await Jimp.read('assets/images/bonzi.png');
-        let blank = await Jimp.read('assets/images/blank.png');
+      let bonzi = await Jimp.read('assets/images/bonzi.png');
+      let blank = await Jimp.read('assets/images/blank.png');
 
-        let font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+      let font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
 
-        blank.resize(175, 120);
-        let fact = blank.print(font, 0, 0, text, 175);
+      blank.resize(175, 120);
+      let fact = blank.print(font, 0, 0, text, 175);
 
-        bonzi.composite(fact, 23, 12);
-        bonzi.getBuffer(Jimp.MIME_PNG, async(err, buffer) => {
-            await message.channel.send({
-                files: [{
-                    name: 'bonzi.png',
-                    attachment: buffer
-                }]
-            })
-            await message.channel.stopTyping()
+      bonzi.composite(fact, 23, 12);
+      bonzi.getBuffer(Jimp.MIME_PNG, async(err, buffer) => {
+        await message.channel.send({
+          files: [{
+            name: 'bonzi.png',
+            attachment: buffer
+          }]
         })
+        await message.channel.stopTyping()
+      })
 
-        return null;
-    }
+    return null;
+  }
 }
 
 module.exports = Bonzi;
