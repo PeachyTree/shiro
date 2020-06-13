83c6a96de2520bfeec665ef01564f3bf1f7d07d4
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:08:14 +0200

    Fix

--- commands/Image/image.js
+++ commands/Image/image.js
@@ -4,32 +4,32 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class Image extends Command {
-    constructor(client) {
-      super(client, {
-        name: "image",
-        description: "Returns a random image.",
-        category: "Image",
-        usage: "image [size (e.g. 1920x1080)]",
-        aliases: ["randomimage", "random-image"]
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "image",
+      description: "Returns a random image.",
+      category: "Image",
+      usage: "image [size (e.g. 1920x1080)]",
+      aliases: ["randomimage", "random-image"]
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        let size = args[0];
-        if (!args[0]) size = "";
+  async run(message, args, level, settings) { 
+    let size = args[0];
+    if (!args[0]) size = "";
 
-        message.channel.startTyping();
+    message.channel.startTyping();
 
-        fetch(`https://source.unsplash.com/random/${size}`)
-            .then(res => message.channel.send({ files: [{ attachment: res.body, name: "image.jpg" }] })
-            .catch(error => {
-                this.client.logger.error(error);
-                message.channel.stopTyping(true);
-                return message.channel.send(`An error occurred: ${error.message}`);
-            }));
+    fetch(`https://source.unsplash.com/random/${size}`)
+    .then(res => message.channel.send({ files: [{ attachment: res.body, name: "image.jpg" }] })
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    }));
 
-        message.channel.stopTyping(true);
-    }
+    message.channel.stopTyping(true);
+  }
 }
 
 module.exports = Image;
