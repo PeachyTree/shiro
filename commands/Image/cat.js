dc5ed33f8b5a7162c2dec93a53965801dc570b1c
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:07:50 +0200

    Add startTyping()

--- commands/Image/cat.js
+++ commands/Image/cat.js
@@ -4,29 +4,29 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class Cat extends Command {
-    constructor(client) {
-      super(client, {
-        name: "cat",
-        description: "Sends a random image of a cat.",
-        category: "Image",
-        usage: "cat"
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "cat",
+      description: "Sends a random image of a cat.",
+      category: "Image",
+      usage: "cat"
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        message.channel.startTyping();
+  async run(message, args, level, settings) { 
+    message.channel.startTyping();
 
-        fetch("https://aws.random.cat/meow")
-            .then(res => res.json())
-            .then(data => message.channel.send({ file: data.file }))
-            .catch(error => {
-                this.client.logger.error(error);
-                message.channel.stopTyping(true);
-                return message.channel.send(`An error occurred: ${error.message}`);
-            });
+    fetch("https://aws.random.cat/meow")
+    .then(res => res.json())
+    .then(data => message.channel.send({ file: data.file }))
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
 
-        message.channel.stopTyping(true);
-    }
+    message.channel.stopTyping(true);
+  }
 }
 
 module.exports = Cat;
--- commands/Image/dog.js
+++ commands/Image/dog.js
@@ -4,29 +4,29 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class Dog extends Command {
-    constructor(client) {
-      super(client, {
-        name: "dog",
-        description: "Sends a random image of a dog.",
-        category: "Image",
-        usage: "dog"
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "dog",
+      description: "Sends a random image of a dog.",
+      category: "Image",
+      usage: "dog"
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        message.channel.startTyping();
+  async run(message, args, level, settings) { 
+    message.channel.startTyping();
 
-        fetch("https://dog.ceo/api/breeds/image/random")
-            .then(res => res.json())
-            .then(data => message.channel.send({ file: data.message }))
-            .catch(error => {
-                this.client.logger.error(error);
-                message.channel.stopTyping(true);
-                return message.channel.send(`An error occurred: ${error.message}`);
-            });
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => message.channel.send({ file: data.message }))
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
 
-        message.channel.stopTyping(true);
-    }
+    message.channel.stopTyping(true);
+  }
 }
 
 module.exports = Dog;
