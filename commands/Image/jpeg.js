79d7c0b3c9c8fbf105f64a40d1424e4208eaf1f0
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:08:31 +0200

    Various bug fixes

--- commands/Image/jpeg.js
+++ commands/Image/jpeg.js
@@ -4,35 +4,35 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class JPEG extends Command {
-    constructor(client) {
-      super(client, {
-        name: "jpeg",
-        description: "Needs more JPEG.",
-        category: "Image",
-        usage: "jpeg <IMAGE_URL>",
-        aliases: ["jpegify"]
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "jpeg",
+      description: "Needs more JPEG.",
+      category: "Image",
+      usage: "jpeg <IMAGE_URL>",
+      aliases: ["jpegify"]
+    });
+  }
 
-    async run(message, args, level, settings) { 
-      const url = args[0] ? args[0].replace(/<(.+)>/g, "$1") : null;
-      if (!url || !url.startsWith("http")) return message.react('🚫'), message.channel.send("Command Usage: `jpeg <IMAGE_URL>`");
+  async run(message, args, level, settings) { 
+    const url = args[0] ? args[0].replace(/<(.+)>/g, "$1") : null;
+    if (!url || !url.startsWith("http")) return message.react('🚫'), message.channel.send("Command Usage: `jpeg <IMAGE_URL>`");
 
-      message.channel.startTyping();
+    message.channel.startTyping();
 
-      fetch(`https://nekobot.xyz/api/imagegen?type=jpeg&url=${url}`)
-        .then(res => res.json())
-        .then(data => {
-          if (!data.success) return message.channel.send("An error occurred. Please ensure the URL you're providing is an image URL.");
-          message.channel.stopTyping(true);
-          return message.channel.send({ file: data.message });
-        })
-        .catch(error => {
-          this.client.logger.error(error);
-          message.channel.stopTyping(true);
-          return message.channel.send(`An error occurred: ${error.message}`);
-        });
-    }
+    fetch(`https://nekobot.xyz/api/imagegen?type=jpeg&url=${url}`)
+    .then(res => res.json())
+    .then(data => {
+      if (!data.success) return message.channel.send("🚫 | An error occurred. Please ensure the URL you're providing is an image URL.");
+      message.channel.stopTyping(true);
+      return message.channel.send({ file: data.message });
+    })
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
+  }
 }
 
 module.exports = JPEG;
