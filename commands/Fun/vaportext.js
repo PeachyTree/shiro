2ff71af238a52fa68d200a9f884ad096105edc66
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:07:24 +0200

    Several bug fixes

--- commands/Fun/vaportext.js
+++ commands/Fun/vaportext.js
@@ -3,26 +3,26 @@
 const Command = require("../../base/Command.js");
 
 class Vaportext extends Command {
-    constructor(client) {
-      super(client, {
-        name: "vaportext",
-        description: "Make text **A E S T H E T I C**.",
-        category: "Fun",
-        usage: "vaportext <Text>",
-        aliases: ["vapor", "vapour", "vapourtext"]
-      });
+  constructor(client) {
+    super(client, {
+      name: "vaportext",
+      description: "Make text **A E S T H E T I C**.",
+      category: "Fun",
+      usage: "vaportext <Text>",
+      aliases: ["vapor", "vapour", "vapourtext"]
+    });
+  }
+
+  async run(message, args, level) { 
+    if (!args.length) return message.react('🚫'), message.channel.send("Command Usage: `vaportext <Text>`");
+
+    let msg = "";
+    for (let i = 0; i < args.length; i++) {
+      msg += args[i].toUpperCase().split("").join(" ") + "   ";
     }
 
-    async run(message, args, level) { 
-        if (!args.length) return message.channel.send("Command Usage: `vaportext <Text>`");
-
-        let msg = "";
-        for (let i = 0; i < args.length; i++) {
-            msg += args[i].toUpperCase().split("").join(" ") + "   ";
-        }
-
-        return message.channel.send(msg);
-    }
+    return message.channel.send(msg);
+  }
 }
 
 module.exports = Vaportext;
