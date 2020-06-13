3a55a37f7b78cfac5f4d5500409f2d9d9ae901a8
Azura Apple <dragonminner867@outlook.com>
2020-06-13 17:51:05 +0200

    Various bug fixes

--- commands/Info/emoji.js
+++ commands/Info/emoji.js
@@ -8,21 +8,21 @@ class Emoji extends Command {
         name: "emoji",
         description: "Displays information about the specified emoji.",
         category: "Info",
-        usage: "emoji <emoji>",
+        usage: "emoji <Emoji>",
         aliases: ["emoji-info", "einfo"]
       });
     }
 
     async run(message, args, level, settings) { 
-        if (!args[0]) return message.react('🚫'), message.reply('You must provide an emoji for me to look up.');
-        if (args[0].startsWith("<a:")) return message.react('🚫'), message.reply(":warning: This command does not support animated emojis yet.");
-        if (args[0].charCodeAt(0) >= 55296) return message.react('🚫'), message.reply(`${args[0]} is a regular Discord emoji, from Twemoji.\nhttps://twemoji.twitter.com`);
+        if (!args[0]) return message.react('🚫'), message.reply('Command Usage: `emoji <Emoji>`');
+        if (args[0].startsWith("<a:")) return message.reply(":warning: This command does not support animated emojis yet.");
+        if (args[0].charCodeAt(0) >= 55296) return message.reply(`${args[0]} is a regular Discord emoji, from Twemoji.\nhttps://twemoji.twitter.com`);
 
         const match = args[0].match(/<:[a-zA-Z0-9_-]+:(\d{18})>/);
-        if (!match || !match[1]) return message.react('🚫'), message.reply('You must provide a valid emoji, from a server I am on.');
+        if (!match || !match[1]) return message.reply('You must provide a valid emoji, from a server I am on.');
 
         const emoji = this.client.emojis.get(match[1]);
-        if (!emoji) return message.react('🚫'), message.reply('You must provide a valid emoji, from a server I am on.');
+        if (!emoji) return message.reply('You must provide a valid emoji, from a server I am on.');
 
         const embed = new RichEmbed()
             .setColor('RANDOM')
@@ -33,7 +33,6 @@ class Emoji extends Command {
             .addField("❯ Created", moment.utc(emoji.createdAt).format("DD/MM/YYYY"), true)
             .addField("❯ From", emoji.guild, true)
             .setFooter(`Requested by ${message.author.tag} using ${settings.prefix}emojiinfo`, message.author.avatarURL);
-
         return message.channel.send({ embed });
     }
 }
