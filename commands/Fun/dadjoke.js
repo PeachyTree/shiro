2000ab46e28d5a9ef00c8bf57b2e3dabbc82cc0b
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:06:03 +0200

    Several bug fixes

--- commands/Fun/dadjoke.js
+++ commands/Fun/dadjoke.js
@@ -4,27 +4,27 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class DadJoke extends Command {
-    constructor(client) {
-      super(client, {
-        name: "dadjoke",
-        description: "Sends a random dad joke.",
-        category: "Fun",
-        usage: "dadjoke",
-        aliases: ["dad", "dadj", "badjoke"]
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "dadjoke",
+      description: "Sends a random dad joke.",
+      category: "Fun",
+      usage: "dadjoke",
+      aliases: ["dad", "dadj", "badjoke"]
+    });
+  }
 
-    async run(message, args, level, settings) { 
-      const meta = { "Accept": "text/plain" };
+  async run(message, args, level, settings) { 
+    const meta = { "Accept": "text/plain" };
 
-      fetch("https://icanhazdadjoke.com/", { headers: meta })
-        .then(res => res.text())
-        .then(body => message.channel.send(body))
-        .catch(error => {
-          this.client.logger.error(error);
-          return message.channel.send(`🚫 | An Error occurred: ${error.message}`);
-        });
-    }
+    fetch("https://icanhazdadjoke.com/", { headers: meta })
+    .then(res => res.text())
+    .then(body => message.channel.send(body))
+    .catch(error => {
+      this.client.logger.error(error);
+      return message.channel.send(`🚫 | An Error occurred: ${error.message}`);
+    });
+  }
 }
 
 module.exports = DadJoke;
