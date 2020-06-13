a16a6b674f7c80222f22fde3a81dd560a0af7f68
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:09:06 +0200

    Multiple bug fixes

--- commands/Image/snake.js
+++ commands/Image/snake.js
@@ -6,50 +6,45 @@ const { RichEmbed } = require("discord.js");
 const { UNSPLASH_ACCESS_KEY } = process.env;
 
 class Snake extends Command {
-    constructor(client) {
-      super(client, {
-        name: "snake",
-        description: "Sends a random image of a snake.",
-        category: "Image",
-        usage: "snake",
-        aliases: ["snek"]
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "snake",
+      description: "Sends a random image of a snake.",
+      category: "Image",
+      usage: "snake",
+      aliases: ["snek"]
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        const page = Math.floor(Math.random() * 5) + 1;
-        const index = Math.floor(Math.random() * 10) + 1;
+  async run(message, args, level, settings) { 
+    const page = Math.floor(Math.random() * 5) + 1;
+    const index = Math.floor(Math.random() * 10) + 1;
+    const meta = { "Authorization": `Client-ID ${UNSPLASH_ACCESS_KEY}` };
 
-        const randomColor = "#0000".replace(/0/g, () => {
-          return (~~(Math.random() * 16)).toString(16);
-        });
+    message.channel.startTyping();
 
-        const meta = { "Authorization": `Client-ID ${UNSPLASH_ACCESS_KEY}` };
+    fetch(`https://api.unsplash.com/search/photos?page=${page}&query=snake`, { headers: meta })
+    .then(res => res.json())
+    .then(json => {
+      const data = json.results[parseInt(index.toFixed(0))];
+      const embed = new RichEmbed()
+        .setTitle("🐍 Snake")
+        .setURL(data.urls.raw)
+        .setDescription(`Photo by [${data.user.name}](${data.user.links.html}) on [Unsplash](https://unsplash.com)`)
+        .setImage(data.urls.raw)
+        .setColor('RANDOM')
+        .setTimestamp();
+      message.channel.send({ embed });
+    })
+    .catch(error => {
+      message.channel.stopTyping(true);
+      if (error.message === "Cannot read property 'urls' of undefined") return message.channel.send('No results found.');
+      this.client.logger.error(error);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
 
-        message.channel.startTyping();
-
-        fetch(`https://api.unsplash.com/search/photos?page=${page}&query=snake`, { headers: meta })
-          .then(res => res.json())
-          .then(json => {
-            const data = json.results[parseInt(index.toFixed(0))];
-            const embed = new RichEmbed()
-              .setTitle("🐍 Snake")
-              .setURL(data.urls.raw)
-              .setDescription(`Photo by [${data.user.name}](${data.user.links.html}) on [Unsplash](https://unsplash.com)`)
-              .setImage(data.urls.raw)
-              .setColor(randomColor)
-              .setTimestamp();
-            message.channel.send({ embed });
-          })
-          .catch(error => {
-            message.channel.stopTyping(true);
-            if (error.message === "Cannot read property 'urls' of undefined") return message.channel.send('No results found.');
-            this.client.logger.error(error);
-            return message.channel.send(`An error occurred: ${error.message}`);
-          });
-
-        message.channel.stopTyping(true);
-    }
+    message.channel.stopTyping(true);
+  }
 }
 
 module.exports = Snake;
