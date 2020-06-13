e0fc3635516c25624b6443d0c2dea1395652f82d
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:06:20 +0200

    Several bug fixes

--- commands/Fun/lmgtfy.js
+++ commands/Fun/lmgtfy.js
@@ -3,24 +3,24 @@
 const Command = require("../../base/Command.js");
 
 class LMGTFY extends Command {
-    constructor(client) {
-      super(client, {
-        name: "lmgtfy",
-        description: "Why don't you just... Google it?",
-        category: "Fun",
-        usage: "lmgtfy <Query>",
-        aliases: ["googleit"]
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "lmgtfy",
+      description: "Why don't you just... Google it?",
+      category: "Fun",
+      usage: "lmgtfy <Query>",
+      aliases: ["googleit"]
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        const textQuery = args.join(" ");
-        const query = encodeURIComponent(args.join(" "));
-        const url = `https://lmgtfy.com/?q=${query}`;
+  async run(message, args, level, settings) { 
+    const textQuery = args.join(" ");
+    const query = encodeURIComponent(args.join(" "));
+    const url = `https://lmgtfy.com/?q=${query}`;
 
-        if (!query) return message.react('🚫'), message.channel.send('Command Usage: `lmgtfy <Query>`');
-        else message.channel.send(`"${textQuery}"\n**<${url}>**`);
-    }
+    if (!query) return message.react('🚫'), message.channel.send('Command Usage: `lmgtfy <Query>`');
+    else message.channel.send(`"${textQuery}"\n**<${url}>**`);
+  }
 }
 
 module.exports = LMGTFY;
