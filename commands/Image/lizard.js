7ea2a610788e0f620d14760b6bf8ac7bb6eab6f4
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:08:39 +0200

    Fix message.content to args

--- commands/Image/lizard.js
+++ commands/Image/lizard.js
@@ -4,29 +4,29 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class Lizard extends Command {
-    constructor(client) {
-      super(client, {
-        name: "lizard",
-        description: "Sends a random image of a lizard.",
-        category: "Image",
-        usage: "lizard"
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "lizard",
+      description: "Sends a random image of a lizard.",
+      category: "Image",
+      usage: "lizard"
+    });
+  }
 
-    async run(message, args, level, settings) { 
-        message.channel.startTyping();
+  async run(message, args, level, settings) { 
+    message.channel.startTyping();
 
-        fetch("https://nekos.life/api/v2/img/lizard")
-            .then(res => res.json())
-            .then(data => message.channel.send({ file: data.url }))
-            .catch(error => {
-                this.client.logger.error(error);
-                message.channel.stopTyping(true);
-                return message.channel.send(`An error occurred: ${error.message}`);
-            });
+    fetch("https://nekos.life/api/v2/img/lizard")
+    .then(res => res.json())
+    .then(data => message.channel.send({ file: data.url }))
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
 
-        message.channel.stopTyping(true);
-    }
+    message.channel.stopTyping(true);
+  }
 }
 
 module.exports = Lizard;
