b3819df9c2760a460e0acc143c2a1c43db5f154a
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:08:44 +0200

    Fix message.content to args

--- commands/Image/magik.js
+++ commands/Image/magik.js
@@ -4,34 +4,34 @@ const Command = require("../../base/Command.js");
 const fetch = require("node-fetch");
 
 class Magik extends Command {
-    constructor(client) {
-      super(client, {
-        name: "magik",
-        description: "Adds a \"magik\" effect to the specified image.",
-        category: "Image",
-        usage: "magik <IMAGE_URL>"
-      });
-    }
+  constructor(client) {
+    super(client, {
+      name: "magik",
+      description: "Adds a \"magik\" effect to the specified image.",
+      category: "Image",
+      usage: "magik <IMAGE_URL>"
+    });
+  }
 
-    async run(message, args, level, settings) { 
-      const url = args[0] ? args[0].replace(/<(.+)>/g, "$1") : null;
-      if (!url || !url.startsWith("http")) return message.react('🚫'), message.channel.send("Command Usage: `magik <IMAGE_URL>`");
+  async run(message, args, level, settings) { 
+    const url = args[0] ? args[0].replace(/<(.+)>/g, "$1") : null;
+    if (!url || !url.startsWith("http")) return message.react('🚫'), message.channel.send("Command Usage: `magik <IMAGE_URL>`");
 
-      message.channel.startTyping();
+    message.channel.startTyping();
 
-      fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${url}`)
-        .then(res => res.json())
-        .then(data => {
-          if (!data.success) return message.channel.send("An error occurred. Please ensure the URL you're providing is an image URL.");
-          message.channel.stopTyping(true);
-          return message.channel.send({ file: data.message });
-        })
-        .catch(error => {
-          this.client.logger.error(error);
-          message.channel.stopTyping(true);
-          return message.channel.send(`An error occurred: ${error.message}`);
-        });
-    }
+    fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${url}`)
+    .then(res => res.json())
+    .then(data => {
+      if (!data.success) return message.channel.send("🚫 | An error occurred. Please ensure the URL you're providing is an image URL.");
+      message.channel.stopTyping(true);
+      return message.channel.send({ file: data.message });
+    })
+    .catch(error => {
+      this.client.logger.error(error);
+      message.channel.stopTyping(true);
+      return message.channel.send(`🚫 | An error occurred: ${error.message}`);
+    });
+  }
 }
 
 module.exports = Magik;
