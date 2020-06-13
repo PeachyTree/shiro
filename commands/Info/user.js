4d704997f63bf9b6181954bd11ed6bc80a784257
Azura Apple <dragonminner867@outlook.com>
2020-06-13 20:10:18 +0200

    Fix

--- commands/Info/user.js
+++ commands/Info/user.js
@@ -4,67 +4,67 @@ const { stripIndents } = require("common-tags");
 const moment = require("moment");
 
 class User extends Command {
-    constructor(client) {
-      super(client, {
-        name: "user",
-        description: "Displays information about the mentioned user.",
-        category: "Info",
-        usage: "user [@USER_MENTION]",
-        aliases: ["userinfo", "whois"],
-        guildOnly: true
-      });
+  constructor(client) {
+    super(client, {
+      name: "user",
+      description: "Displays information about the mentioned user.",
+      category: "Info",
+      usage: "user [@USER_MENTION]",
+      aliases: ["userinfo", "whois"],
+      guildOnly: true
+    });
+  }
+
+  async run(message, args, level, settings) { 
+    if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
+  
+    const user = message.mentions.users.first() || message.author;
+
+    let status;
+    if (user.presence.status.toProperCase() === "Dnd") {
+      status = "Do Not Disturb";
+    } else {
+      status = user.presence.status.toProperCase();
     }
 
-    async run(message, args, level, settings) { 
-        if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
+    let activity = "";
+
+    if (!user.presence.game) {
+      activity = "Nothing";
+    }
+
+    if (user.presence.game) {
+      activity = `Playing **${user.presence.game ? user.presence.game.name : "Nothing"}**`;
+    }
+
+    if (user.presence.game.name === "Spotify") {
+      activity = "Listening to **Spotify**";
+    }
+
+    const embed = new RichEmbed()
+      .setColor(message.guild.member(user).displayColor)
+      .setThumbnail(user.displayAvatarURL)
+      .setTitle(`User Information for ${user.tag}`)
+      .setDescription(`**User ID**: ${user.id}`)
       
-        const user = message.mentions.users.first() || message.author;
+      .addField("❯ Details", stripIndents`
+      • Status: **${status}**
+      • Activity: ${activity}
+      ‍   
+      `, true)
 
-        let status;
-        if (user.presence.status.toProperCase() === "Dnd") {
-          status = "Do Not Disturb";
-        } else {
-          status = user.presence.status.toProperCase();
-        }
+      .addField("❯ Roles", stripIndents`
+      • Highest: **\`${message.guild.member(user).highestRole.name}\`**
+      • All: ${message.guild.member(user).roles.map(roles => `\`${roles.name}\``).slice(1).join(", ")}
+      ‍   
+      `, true)
 
-        let activity = "";
-
-        if (!user.presence.game) {
-          activity = "Nothing";
-        }
-
-        if (user.presence.game) {
-          activity = `Playing **${user.presence.game ? user.presence.game.name : "Nothing"}**`;
-        }
-
-        if (user.presence.game.name === "Spotify") {
-          activity = "Listening to **Spotify**";
-        }
-
-        const embed = new RichEmbed()
-          .setColor(message.guild.member(user).displayColor)
-          .setThumbnail(user.displayAvatarURL)
-          .setTitle(`User Information for ${user.tag}`)
-          .setDescription(`**User ID**: ${user.id}`)
-
-          .addField("❯ Details", stripIndents`
-          • Status: **${status}**
-          • Activity: ${activity}
-          ‍   
-          `, true)
-
-          .addField("❯ Roles", stripIndents`
-          • Highest: **\`${message.guild.member(user).highestRole.name}\`**
-          • All: ${message.guild.member(user).roles.map(roles => `\`${roles.name}\``).slice(1).join(", ")}
-          ‍   
-          `, true)
-
-          .addField("❯ Join Dates", stripIndents`
-          • ${message.guild.name}: **${moment.utc(message.guild.member(user).joinedAt).format("dddd, Do MMMM YYYY @ HH:mm:ss")}**
-          • Discord: **${moment.utc(user.createdAt).format("dddd, Do MMMM YYYY @ HH:mm:ss")}**
-          `, true)
-        message.channel.send({ embed });
-    }
+      .addField("❯ Join Dates", stripIndents`
+      • ${message.guild.name}: **${moment.utc(message.guild.member(user).joinedAt).format("dddd, Do MMMM YYYY @ HH:mm:ss")}**
+      • Discord: **${moment.utc(user.createdAt).format("dddd, Do MMMM YYYY @ HH:mm:ss")}**
+      `, true)
+    message.channel.send({ embed });
+  }
 }
 
 module.exports = User;
