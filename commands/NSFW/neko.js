5b72ec68e5196dee5696cee82a31af1e2c6a877c
Azura Apple <dragonminner867@outlook.com>
2020-06-22 08:47:30 +0200

    Use superfetch instead of snekfetch

--- commands/NSFW/neko.js
+++ commands/NSFW/neko.js
@@ -1,13 +1,11 @@
-// Copyright (c) 2020 Azura Apple. All rights reserved. MIT license.
-
 const Command = require('../../base/Command.js');
 const { RichEmbed } = require('discord.js');
-const snekfetch = require('snekfetch');
+const request = require('node-superfetch');
 
 class Neko extends Command {
     constructor(client) {
         super(client, {
-            name: 'cry',
+            name: 'neko',
             description: 'Nekos!\nThis command is NSFW in NSFW channels and not NSFW in normal channels!',
             category: 'NSFW',
             usage: 'neko',
@@ -18,7 +16,7 @@ class Neko extends Command {
 
     async run(message, args, level, settings) {
         if (!message.channel.nsfw) {
-            const res = await snekfetch.get(`http://nekos.life/api/neko`);
+            const res = await request.get('http://nekos.life/api/neko');
             const preview = res.body.neko;
 
             const embed = new RichEmbed()
@@ -28,7 +26,7 @@ class Neko extends Command {
             return message.channel.send({ embed });
 
         } else {
-            const res = await snekfetch.get(`http://nekos.life/api/lewd/neko`);
+            const res = await request.get('http://nekos.life/api/lewd/neko');
             const preview = res.body.neko;
 
             const embed = new RichEmbed()
