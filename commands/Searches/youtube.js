97d2f6cd06ebe960e74f98cd694e927bfaa0ef85
Azura Apple <dragonminner867@outlook.com>
2020-06-22 08:48:15 +0200

    12x Fixes

--- commands/Searches/youtube.js
+++ commands/Searches/youtube.js
@@ -1,5 +1,3 @@
-// Copyright (c) 2020 Azura Apple. All rights reserved. MIT license.
-
 const Command = require('../../base/Command.js');
 let youtube_node = require('youtube-node');
 youtube = new youtube_node();
