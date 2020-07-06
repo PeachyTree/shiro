70f952b1f145b2f70048eaee4704a8c37ce9c732
Azura Apple <dragonminner867@outlook.com>
2020-07-06 21:51:12 +0200

    Determine files / lines of codes

--- util/Utils.js
+++ util/Utils.js
@@ -4,6 +4,10 @@ class Utils {
         if (mode === "decode") return Buffer.from(text, "base64").toString("utf8") || null;
         throw new TypeError(`${mode} is not a supported Base64 mode`);
     }
+
+    static formatNumberK(number) {
+		return number > 999 ? `${(number / 1000).toLocaleString(undefined, { maximumFractionDigits: 1 })}K` : number;
+	}
 }
 
 module.exports = Utils;
