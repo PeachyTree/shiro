1c7d52a22b05ccca5e38fa7d8040356b4cd4ebe5
Azura Apple <dragonminner867@outlook.com>
2020-07-07 12:48:48 +0200

    Fix

--- commands/Searches/wikipedia.js
+++ commands/Searches/wikipedia.js
@@ -19,33 +19,33 @@ class Wikipedia extends Command {
     }
 
     try {
-      const query = args.join(" ");
-			const { body } = await request
-				.get('https://en.wikipedia.org/w/api.php')
-				.query({
-					action: 'query',
-					prop: 'extracts|pageimages',
-					format: 'json',
-					titles: query,
-					exintro: '',
-					explaintext: '',
-					pithumbsize: 150,
-					redirects: '',
-					formatversion: 2
-				});
-			const data = body.query.pages[0];
-			if (data.missing) return msg.say('Could not find any results.');
-			const embed = new MessageEmbed()
-				.setColor('RANDOM')
-				.setTitle(data.title)
-				.setAuthor('Wikipedia', 'https://i.imgur.com/Z7NJBK2.png', 'https://www.wikipedia.org/')
-				.setThumbnail(data.thumbnail ? data.thumbnail.source : null)
-				.setURL(`https://en.wikipedia.org/wiki/${encodeURIComponent(query).replace(/\)/g, '%29')}`)
-				.setDescription(shorten(data.extract.replace(/\n/g, '\n\n')));
-			return message.channel.send({ embed });
-		} catch (err) {
-			return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
-		}
+      	const query = args.join(" ");
+		const { body } = await request
+		.get('https://en.wikipedia.org/w/api.php')
+		.query({
+			action: 'query',
+			prop: 'extracts|pageimages',
+			format: 'json',
+			titles: query,
+			exintro: '',
+			explaintext: '',
+			pithumbsize: 150,
+			redirects: '',
+			formatversion: 2
+		});
+		const data = body.query.pages[0];
+		if (data.missing) return msg.say('Could not find any results.');
+		const embed = new MessageEmbed()
+			.setColor('RANDOM')
+			.setTitle(data.title)
+			.setAuthor('Wikipedia', 'https://i.imgur.com/Z7NJBK2.png', 'https://www.wikipedia.org/')
+			.setThumbnail(data.thumbnail ? data.thumbnail.source : null)
+			.setURL(`https://en.wikipedia.org/wiki/${encodeURIComponent(query).replace(/\)/g, '%29')}`)
+			.setDescription(shorten(data.extract.replace(/\n/g, '\n\n')));
+		return message.channel.send({ embed });
+	} catch (err) {
+		return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
+	}
   };
 }
 
