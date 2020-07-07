ffbd60c5f531b8e8317b5402803f3bfb8a1baefd
Azura Apple <dragonminner867@outlook.com>
2020-07-07 12:51:09 +0200

    Fix

--- commands/Searches/wikipedia.js
+++ commands/Searches/wikipedia.js
@@ -3,50 +3,50 @@ const request = require('node-superfetch');
 const { shorten } = require('../../util/Utils.js');
 
 class Wikipedia extends Command {
-  constructor(client) {
-    super(client, {
-      name: "wikipedia",
-      description: "Searches Wikipedia for the specified article.",
-      category: "Searches",
-      usage: "wikipedia <Query>"
-    });
-  }
+  	constructor(client) {
+    	super(client, {
+		name: "wikipedia",
+		description: "Searches Wikipedia for the specified article.",
+		category: "Searches",
+		usage: "wikipedia <Query>"
+    	});
+  	}
 
-  async run(message, args) { 
+  	async run(message, args) { 
 
-    if (!args.length) {
-      return message.reply("Command Usage: `wikipedia <text>`")
-    }
+		if (!args.length) {
+			return message.reply("Command Usage: `wikipedia <text>`")
+		}
 
-    try {
-      	const query = args.join(" ");
-		const { body } = await request
-		.get('https://en.wikipedia.org/w/api.php')
-		.query({
-			action: 'query',
-			prop: 'extracts|pageimages',
-			format: 'json',
-			titles: query,
-			exintro: '',
-			explaintext: '',
-			pithumbsize: 150,
-			redirects: '',
-			formatversion: 2
-		});
-		const data = body.query.pages[0];
-		if (data.missing) return msg.say('Could not find any results.');
-		const embed = new MessageEmbed()
-			.setColor('RANDOM')
-			.setTitle(data.title)
-			.setAuthor('Wikipedia', 'https://i.imgur.com/Z7NJBK2.png', 'https://www.wikipedia.org/')
-			.setThumbnail(data.thumbnail ? data.thumbnail.source : null)
-			.setURL(`https://en.wikipedia.org/wiki/${encodeURIComponent(query).replace(/\)/g, '%29')}`)
-			.setDescription(shorten(data.extract.replace(/\n/g, '\n\n')));
-		return message.channel.send({ embed });
-	} catch (err) {
-		return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
-	}
-  };
+		try {
+			const query = args.join(" ");
+			const { body } = await request
+			.get('https://en.wikipedia.org/w/api.php')
+			.query({
+				action: 'query',
+				prop: 'extracts|pageimages',
+				format: 'json',
+				titles: query,
+				exintro: '',
+				explaintext: '',
+				pithumbsize: 150,
+				redirects: '',
+				formatversion: 2
+			});
+			const data = body.query.pages[0];
+			if (data.missing) return msg.say('Could not find any results.');
+			const embed = new MessageEmbed()
+				.setColor('RANDOM')
+				.setTitle(data.title)
+				.setAuthor('Wikipedia', 'https://i.imgur.com/Z7NJBK2.png', 'https://www.wikipedia.org/')
+				.setThumbnail(data.thumbnail ? data.thumbnail.source : null)
+				.setURL(`https://en.wikipedia.org/wiki/${encodeURIComponent(query).replace(/\)/g, '%29')}`)
+				.setDescription(shorten(data.extract.replace(/\n/g, '\n\n')));
+			return message.channel.send({ embed });
+		} catch (err) {
+			return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
+		}
+	};
 }
 
 module.exports = Wikipedia;
