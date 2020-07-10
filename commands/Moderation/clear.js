af137344d900b9877bde714ad1bd66ae03a79d52
Azura Apple <dragonminner867@outlook.com>
2020-07-10 14:08:38 +0200

    Prune alias

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -1,26 +1,27 @@
 const Command = require('../../base/Command.js');
 
-class Purge extends Command {
+class Clear extends Command {
   constructor(client) {
     super(client, {
       name: "clear",
       description: "Clears (bulk-deletes) between 2 and 99 messages.",
       category: "Moderation",
       usage: "clear [@USER_MENTION] <Number>",
+      aliases: ["prune"],
       permLevel: "Moderator",
       guildOnly: true
     });
   }
 
-  async run(message, args, level, settings) { 
+  async run(message) { 
     if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
-    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`🚫 | I cannot run this command as I have insufficient permissions to do so. Please ensure I have the \"Manage Messages\" permission.`);
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`I cannot run this command as I have insufficient permissions to do so. Please ensure I have the \"Manage Messages\" permission.`);
 
     const user = message.mentions.users.first();
     const amount = parseInt(message.content.split(" ")[1]) ? parseInt(message.content.split(" ")[1]) : parseInt(message.content.split(" ")[2]);
 
-    if (!amount && !user) return message.react('🚫'), message.reply('Command Usage: `clear [@USER_MENTION] <Number>`');
-    if (amount < 2 || amount > 99) return message.channel.send("🚫 | You've provided an invalid number of messages to delete. Please ensure it's between 2 and 99 (inclusive).");
+    if (!amount && !user) return message.reply('Command Usage: `clear [@USER_MENTION] <Number>`');
+    if (amount < 2 || amount > 99) return message.channel.send("You've provided an invalid number of messages to delete. Please ensure it's between 2 and 99 (inclusive).");
 
     let messages = await message.channel.fetchMessages({ limit: amount });
 
@@ -37,7 +38,7 @@ class Purge extends Command {
       })
       .catch(error => {
         this.client.logger.error(error);
-        message.channel.send(`🚫 | An error occurred:\n\```${error.message}\````);
+        message.channel.send(`An error occurred:\n\```${error.message}\````);
       });
     } else {
       this.client.emit("messageDeleteBulk", messages);
@@ -50,12 +51,10 @@ class Purge extends Command {
       .catch(error => {
         if (error.message === "You can only bulk delete messages that are under 14 days old.") return message.channel.send(error.message);
         this.client.logger.error(error);
-        return message.channel.send(`🚫 | An error occurred:\n\```${error.message}\````);
+        return message.channel.send(`An error occurred:\n\```${error.message}\````);
       });
-  
-      //return message.channel.send(`**${amount}** messages were cleared.`);
     }
   }
 }
 
-module.exports = Purge;
+module.exports = Clear;
