b02aa71a3f5412417ab04dda456fc20578ec4071
Azura Apple <dragonminner867@outlook.com>
2020-07-14 12:38:30 +0200

    Remove reaction

--- commands/Bot Owner/reload.js
+++ commands/Bot Owner/reload.js
@@ -11,17 +11,17 @@ class Reload extends Command {
     });
   }
 
-  async run(message, args, level) { 
-    if (!args[0]) return message.react('🚫'), message.reply("You must provide a command to reload.");
+  async run(message, args) { 
+    if (!args[0]) return message.reply("You must provide a command to reload.");
     
     const commands = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
-    if (!commands) return message.reply(`🚫 | The command \`${args[0]}\` does not exist, nor is it an alias.`);
+    if (!commands) return message.reply(`The command \`${args[0]}\` does not exist, nor is it an alias.`);
 
     let response = await this.client.unloadCommand(commands.conf.location, commands.help.name);
-    if (response) return message.channel.send(`🚫 | Error Unloading: \`${response}\`.`);
+    if (response) return message.channel.send(`Error Unloading: \`${response}\`.`);
 
     response = this.client.loadCommand(commands.conf.location, commands.help.name);
-    if (response) return message.channel.send(`🚫 | Error loading: \`${response}\`.`);
+    if (response) return message.channel.send(`Error loading: \`${response}\`.`);
 
     message.channel.send(`☑️ | The command \`${commands.help.name}\` has been reloaded.`);
   }
