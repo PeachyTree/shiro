21a7471b5779b1cf276475ab950d28481e2e6581
Azura Apple <dragonminner867@outlook.com>
2020-07-15 10:20:07 +0200

    Fix

--- commands/Anime/anime.js
+++ commands/Anime/anime.js
@@ -14,49 +14,50 @@ class Anime extends Command {
     });
   }
 
-  async run(message, args) {
-        let search = args.join(" ");
-
-        if (!search) {
-
-            kitsu.searchAnime(aq().quoteanime).then(result => {
-
-                let anime = result[0]
-
-                let embed = new MessageEmbed()
-                    .setColor('RANDOM')
-                    .setAuthor(`${anime.titles.english} | ${anime.showType}`, anime.posterImage.original)
-                    .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
-                    .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
-                    .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
-                    .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
-                    .setImage(anime.posterImage.original);
-                return message.channel.send(`📺 | Try watching **${anime.titles.english}**!`, { embed });
-            })
-
-        } else {
+    async run(message, args) {
+        try {
             let search = args.join(" ");
 
-            kitsu.searchAnime(search).then(result => {
-                if (result.length === 0) {
-                    return message.channel.send(`No search results found for **${search}**!`);
-                }
+            if (!search) {
 
-                let anime = result[0]
+                kitsu.searchAnime(aq().quoteanime).then(result => {
 
-                let embed = new MessageEmbed()
-                    .setColor('RANDOM')
-                    .setAuthor(`${anime.titles.english ? anime.titles.english : search} | ${anime.showType}`, anime.posterImage.original)
-                    .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
-                    .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
-                    .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
-                    .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
-                    .setImage(anime.posterImage.original);
-                return message.channel.send({ embed });
-            }).catch(err => {
-                this.client.logger.log(err)
-                return message.channel.send(`No search results found for **${search}**!`);
-            });
+                    let anime = result[0]
+
+                    let embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setAuthor(`${anime.titles.english} | ${anime.showType}`, anime.posterImage.original)
+                        .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
+                        .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
+                        .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
+                        .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
+                        .setImage(anime.posterImage.original);
+                    return message.channel.send(`📺 | Try watching **${anime.titles.english}**!`, { embed });
+                })
+
+            } else {
+                let search = args.join(" ");
+
+                kitsu.searchAnime(search).then(result => {
+                    if (result.length === 0) {
+                        return message.channel.send(`No search results found for **${search}**!`);
+                    }
+
+                    let anime = result[0]
+
+                    let embed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setAuthor(`${anime.titles.english ? anime.titles.english : search} | ${anime.showType}`, anime.posterImage.original)
+                        .setDescription(anime.synopsis.replace(/<[^>]*>/g, '').split('\n')[0])
+                        .addField('❯\u2000\Information', `•\u2000\**Japanese Name:** ${anime.titles.romaji}\n\•\u2000\**Age Rating:** ${anime.ageRating}\n\•\u2000\**NSFW:** ${anime.nsfw ? 'Yes' : 'No'}`, true)
+                        .addField('❯\u2000\Stats', `•\u2000\**Average Rating:** ${anime.averageRating}\n\•\u2000\**Rating Rank:** ${anime.ratingRank}\n\•\u2000\**Popularity Rank:** ${anime.popularityRank}`, true)
+                        .addField('❯\u2000\Status', `•\u2000\**Episodes:** ${anime.episodeCount ? anime.episodeCount : 'N/A'}\n\•\u2000\**Start Date:** ${anime.startDate}\n\•\u2000\**End Date:** ${anime.endDate ? anime.endDate : "Still airing"}`, true)
+                        .setImage(anime.posterImage.original);
+                    return message.channel.send({ embed });
+                });
+            }
+        } catch (err) {
+            return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
         }
     }
 }
--- commands/Anime/waifu.js
+++ commands/Anime/waifu.js
@@ -15,40 +15,43 @@ class Waifu extends Command {
   }
 
   async run(message, args) {
+    try {
+      let waifuNumber = args.join(" ");
+      if (waifuNumber <= total && waifuNumber > 0) return message.channel.send(`That's not a valid waifu number! There are only **${total}** waifus right now, *choose a number between 1 and ${total}*!`);
 
-        let waifuNumber = args.join(" ");
-        if (waifuNumber <= total && waifuNumber > 0) return message.channel.send(`That's not a valid waifu number! There are only **${total}** waifus right now, *choose a number between 1 and ${total}*!`);
+      if (!waifuNumber) {
+        let random = Math.floor(Math.random() * total + 1);
+        let waifu = waifus[random];
 
-        if (!waifuNumber) {
-            let random = Math.floor(Math.random() * total + 1);
-            let waifu = waifus[random];
+        const embed = new MessageEmbed()
+          .setAuthor(waifu.name, waifu.image)
+          .setDescription(waifu.origin)
+          .setImage(waifu.image)
+          .setFooter(`Waifu Number ${random}`)
+          .setColor('#FAC193');
+        let ms = await message.channel.send(`💝 | **${waifu.name}**? `, { embed });
+        await ms.react('👍');
+        await ms.react('👎');
 
-            const embed = new MessageEmbed()
-                .setAuthor(waifu.name, waifu.image)
-                .setDescription(waifu.origin)
-                .setImage(waifu.image)
-                .setFooter(`Waifu Number ${random}`)
-                .setColor('#FAC193');
-            let ms = await message.channel.send(`💝 | **${waifu.name}**? `, { embed });
-            await ms.react('👍');
-            await ms.react('👎');
-
-            return null;
+        return null;
 
         } else if (waifuNumber) {
-            let waifu = waifus[waifuNumber]
-            const embed = new MessageEmbed()
-                .setAuthor(waifu.name, waifu.image)
-                .setDescription(waifu.origin)
-                .setImage(waifu.image)
-                .setFooter(`Waifu Number ${waifuNumber}`)
-                .setColor('#FAC193');
-            let ms = await message.channel.send(`💝 | Here's waifu number **${waifuNumber}**!`, { embed });
-            await ms.react('👍');
-            await ms.react('👎');
+          let waifu = waifus[waifuNumber]
+          const embed = new MessageEmbed()
+            .setAuthor(waifu.name, waifu.image)
+            .setDescription(waifu.origin)
+            .setImage(waifu.image)
+            .setFooter(`Waifu Number ${waifuNumber}`)
+            .setColor('#FAC193');
+          let ms = await message.channel.send(`💝 | Here's waifu number **${waifuNumber}**!`, { embed });
+          await ms.react('👍');
+          await ms.react('👎');
 
-            return null;
+          return null;
         }
+      } catch (err) {
+        return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
+      }
     }
 }
 
