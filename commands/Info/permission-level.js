d65e3a0c2ab13c89c74d57de22cb5f4e6c0a37d3
Azura Apple <dragonminner867@outlook.com>
2020-07-17 19:31:15 +0200

    Lorem Ipsum, Permission Level Commands

---
+++ commands/Info/permission-level.js
@@ -0,0 +1,21 @@
+const Command = require("../../base/Command.js");
+
+class PermissionLevel extends Command {
+  constructor(client) {
+    super(client, {
+      name: "permission-level",
+      description: "Displays your permission level for your location.",
+      category: "Info",
+      usage: "permission-level",
+      aliases: ["my-level", "perm-level"],
+      guildOnly: true
+    });
+  }
+
+  async run(message, level) {
+    const friendly = this.client.config.permLevels.find(l => l.level === level).name;
+    message.reply(`Your current Permission Level is: **${level}** (${friendly}).`);
+  }
+}
+
+module.exports = PermissionLevel;
---
+++ commands/Random Response/loremipsum.js
@@ -0,0 +1,27 @@
+const Command = require("../../base/Command.js");
+const { get } = require("snekfetch");
+
+class LoremIpsum extends Command {
+    constructor(client) {
+      super(client, {
+        name: "loremipsum",
+        description: "Need placeholder text for your website? Look no further.",
+        category: "Random Response",
+        usage: "loremipsum",
+        aliases: ["placeholder", "lorem", "lorem-ipsum"]
+      });
+    }
+
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        try {
+          const { raw } = await get("https://loripsum.net/api").set("Accept", "text/plain");
+          const text = raw.toString();
+          message.channel.send(text.length >= 2000 ? text.substring(0, 1980) + "... </p>" : text, { code: "html" });
+        } catch (error) {
+          this.client.logger.error(error);
+          return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+        }
+    }
+}
+
+module.exports = LoremIpsum;
