4d0be5b43c67e60c3d99f6104dbe73ba4ab162ae
Snowball <dragonminner867@outlook.com>
2020-09-25 19:04:32 +0200

    Fix

--- celestia.js
+++ celestia.js
@@ -1,4 +1,4 @@
-// Celestia Discord Bot Copyright (©) 2018 - 2020 Snowball. All rights reserved. MIT License.
+// Celestia Discord Bot Copyright (©) 2018 - 2020 Snowy. All rights reserved. MIT License.
 
 require("dotenv").config();
 const Discord = require("discord.js");
