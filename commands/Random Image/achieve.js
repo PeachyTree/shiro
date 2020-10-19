dea8cf6280f9d47b8f1d60cfd8d9c1b44acfd4a4
Fate <53138512+TheFateAkira@users.noreply.github.com>
2020-10-19 21:02:56 +0700

    Update achieve.js

--- commands/Random Image/achieve.js
+++ commands/Random Image/achieve.js
@@ -12,14 +12,14 @@ class Achieve extends Command {
   }
 
 	async run (message, args) {
-      try {
-        if (!args[0]) return message.reply('You need to input somthing to make an achievement!');
+	      try {
+		if (!args[0]) return message.reply('You need to input somthing to make an achievement!');
 
-        message.channel.send(new Discord.MessageAttachment('https://www.minecraftskinstealer.com/achievement/a.php?i=20&h=Achievment+Get!&t=' + encodeURIComponent(args.join(' ')), 'mc.png'))  
-      } catch (err) {
-        message.channel.send('There was an error!\n' + err).catch();
-      }
-    }
-}
+		message.channel.send(new Discord.MessageAttachment('https://www.minecraftskinstealer.com/achievement/a.php?i=20&h=Achievment+Get!&t=' + encodeURIComponent(args.join(' ')), 'mc.png'))  
+	      } catch (err) {
+		message.channel.send('There was an error!\n' + err).catch();
+	      }
+	    }
+	}
 
 module.exports = Achieve;
