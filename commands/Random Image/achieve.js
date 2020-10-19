623fbde3d5f732e53dff8ebeac17582873c18374
Fate <53138512+TheFateAkira@users.noreply.github.com>
2020-10-19 21:01:37 +0700

    Create achieve.js

---
+++ commands/Random Image/achieve.js
@@ -0,0 +1,25 @@
+const Command = require("../../base/Command.js");
+const Discord = require('discord.js');
+
+class Achieve extends Command {
+  	constructor(client) {
+    	super(client, {
+			name: "chieve",
+			description: "Sends a random image of a bird.",
+			category: "Random Image",
+			usage: "achieve"
+    	});
+  	}
+
+	async run (message, args) {
+      try {
+        if (!args[0]) return message.reply('You need to input somthing to make an achievement!');
+
+        message.channel.send(new Discord.MessageAttachment('https://www.minecraftskinstealer.com/achievement/a.php?i=20&h=Achievment+Get!&t=' + encodeURIComponent(args.join(' ')), 'mc.png'))  
+      } catch (err) {
+        message.channel.send('There was an error!\n' + err).catch();
+      }
+    }
+}
+
+module.exports = Achieve;
