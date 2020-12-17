a1060b04221346f82e478d89339003189ad37276
Shin <dragonminner867@outlook.com>
2020-12-17 09:47:52 +0100

    Client clean function

--- modules/functions.js
+++ modules/functions.js
@@ -22,6 +22,20 @@ module.exports = (client) => {
     }
   };
   
+  client.clean = async (client, text) => {
+    if (text && text.constructor.name == "Promise")
+      text = await text;
+    if (typeof evaled !== "string")
+      text = require("util").inspect(text, {depth: 0});
+
+    text = text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203))
+      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+
+    return text;
+  };
+  
   String.prototype.toProperCase = function() {
     return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
   };
