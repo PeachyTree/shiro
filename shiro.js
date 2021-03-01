adbf73e3aab2b30b2553651449f60a45c9924af8
Shin <dragonminner867@outlook.com>
2021-03-01 08:37:50 +0100

    Remove additional logger functions

--- shiro.js
+++ shiro.js
@@ -1,4 +1,4 @@
-// Shiro Discord Bot Copyright (©) 2018 - 2021 ShinWulf#0484. All rights reserved. MIT License.
+// Shiro Discord Bot Copyright (©) 2018 - 2021 Shinwulf. All rights reserved. MIT License.
 
 // We need this for our .env file, make sure to put it above anything else!
 require("dotenv").config();
@@ -154,7 +154,3 @@ const init = async () => {
 };
 
 init();
-
-// Some additional logger functions
-client.on("error", e => client.logger.error(e))
-      .on("warn", info => client.logger.warn(info));
