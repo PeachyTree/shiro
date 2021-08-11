0f44ad8960925f0f4dbd11bbf5de120c55291f23
PeachyTree <dragonminner867@outlook.com>
2021-08-11 21:48:55 +0200

    Activities fix

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const activities = require("../assets/json/bot/activities");
+const activities = require('../assets/json/bot/activities.json');
 
 module.exports = class {
   constructor(client) {
@@ -6,24 +6,21 @@ module.exports = class {
   }
 
   async run() {
-
     await this.client.wait(1000);
-
     this.client.appInfo = await this.client.fetchApplication();
     setInterval( async () => {
       this.client.appInfo = await this.client.fetchApplication();
     }, 60000);
-
-    if (!this.client.settings.has("default")) {
-      if (!this.client.config.defaultSettings) throw new Error("defaultSettings not preset in config.js or settings database. Bot cannot load.");
-      this.client.settings.set("default", this.client.config.defaultSettings);
+    if (!this.client.settings.has('default')) {
+      if (!this.client.config.defaultSettings) throw new Error('defaultSettings not preset in config.js or settings database. Bot cannot load.');
+      this.client.settings.set('default', this.client.config.defaultSettings);
     }
 
     // Activity List
     setInterval(() => { 
       let index = activities[Math.round(Math.random() * (activities.length - 1))];
-      this.client.user.setPresence({ game: { name: index, type: "PLAYING"}});
+      this.client.user.setPresence({ game: { name: index, type: 'PLAYING' }});
   }, 300000); // = 5 minutes
-    this.client.logger.log(`Ready and logged in as ${this.client.user.tag}`, "ready");
+    this.client.logger.log(`Ready and logged in as ${this.client.user.tag}`, 'ready');
   }
 };
