26d2d5473233a79e0b44ab4646d912db089dcd47
PeachyTree <dragonminner867@outlook.com>
2021-08-11 21:49:43 +0200

    Multiple corrections, cleaning...

--- shiro.js
+++ shiro.js
@@ -1,32 +1,34 @@
-// Shiro Discord Bot Copyright (©) 2017 - 2021 Shinwulf. All rights reserved. MIT License.
+/*-------------------------------------------------
+ * Shiro for Discord
+ * Copyright (c) 2017-2021 PeachyTree. All rights reserved.
+ * See LICENSE in project root for license info.
+ *------------------------------------------------*/
 
-require("dotenv").config();
-const { Client } = require("discord.js");
-const { promisify } = require("util");
-const readdir = promisify(require("fs").readdir);
-const Enmap = require("enmap");
-const klaw = require("klaw");
-const path = require("path");
+require('dotenv').config();
+const { Client } = require('discord.js');
+const { promisify } = require('util');
+const readdir = promisify(require('fs').readdir);
+const Enmap = require('enmap');
+const klaw = require('klaw');
+const path = require('path');
 const { SHIRO_TOKEN } = process.env;
 
 class Shiro extends Client {
   constructor(options) {
     super(options);
 
-    this.config = require("./config.js");
+    this.config = require('./config.js');
     this.commands = new Discord.Collection();
     this.aliases = new Discord.Collection();
-    this.settings = new Enmap({ name: "settings" });
-    this.logger = require("./controllers/Logger");
+    this.settings = new Enmap({ name: 'settings' });
+    this.logger = require('./controllers/Logger');
     this.wait = promisify(setTimeout);
     this.cache = null;
   }
 
   permlevel(message) {
     let permlvl = 0;
-
     const permOrder = this.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
-
     while (permOrder.length) {
       const currentLevel = permOrder.shift();
       if (message.guild && currentLevel.guildOnly) continue;
@@ -41,7 +43,7 @@ class Shiro extends Client {
   loadCommand(commandPath, commandName) {
     try {
       const props = new (require(`${commandPath}${path.sep}${commandName}`))(client);
-      this.logger.log(`Loading command: ${props.help.name}. ✔`, "log");
+      this.logger.log(`Loading command: ${props.help.name}. ✔`, 'log');
       props.conf.location = commandPath;
       if (props.init) {
         props.init(this);
@@ -64,7 +66,6 @@ class Shiro extends Client {
       command = this.commands.get(this.aliases.get(commandName));
     }
     if (!command) return `The command \`${commandName}\` doesn't seem to exist, nor is it an alias.`;
-
     if (command.shutdown) {
       await command.shutdown(this);
     }
@@ -76,7 +77,6 @@ class Shiro extends Client {
     if (guild) {
       const defaults = client.config.defaultSettings || {};
       const guildData = client.settings.get(guild.id) || {};
-
       const returnObject = {};
       Object.keys(defaults).forEach((key) => {
         returnObject[key] = guildData[key] ? guildData[key] : defaults[key];
@@ -86,7 +86,7 @@ class Shiro extends Client {
   }
 
   writeSettings(id, newSettings) {
-    const defaults = this.settings.get("default");
+    const defaults = this.settings.get('default');
     let settings = this.settings.get(id);
     if (typeof settings != "object") settings = {};
     for (const key in newSettings) {
@@ -99,44 +99,36 @@ class Shiro extends Client {
     this.settings.set(id, settings);
   }
 }
-
 const client = new Shiro({
-  disabledEvents: ["TYPING_START", "RELATIONSHIP_ADD", "RELATIONSHIP_REMOVE", "CHANNEL_PINS_UPDATE"],
+  disabledEvents: ['TYPING_START', 'RELATIONSHIP_ADD', 'RELATIONSHIP_REMOVE', 'CHANNEL_PINS_UPDATE'],
   disableEveryone: true
 });
-
 console.log(client.config.permLevels.map(p => `${p.level} : ${p.name}`));
-
-require("./controllers/functions.js")(client);
-
+require('./controllers/functions.js')(client);
 const init = async () => {
-  klaw("./commands").on("data", (item) => {
+  klaw('./commands').on('data', (item) => {
     const cmdFile = path.parse(item.path);
-    if (!cmdFile.ext || cmdFile.ext !== ".js") return;
+    if (!cmdFile.ext || cmdFile.ext !== '.js') return;
     const response = client.loadCommand(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`);
     if (response) client.logger.error(response);
   });
-
-  const evtFiles = await readdir("./events");
-  client.logger.log(`Loading a total of ${evtFiles.length} events.`, "log");
+  const evtFiles = await readdir('./events');
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`, 'log');
   evtFiles.forEach(file => {
-    const eventName = file.split(".")[0];
+    const eventName = file.split('.')[0];
     client.logger.log(`Loading event: ${eventName}. ✅`);
     const event = new (require(`./events/${file}`))(client); 
-
     client.on(eventName, (...args) => event.run(...args));
     const mod = require.cache[require.resolve(`./events/${file}`)];
     delete require.cache[require.resolve(`./events/${file}`)];
     const index = mod.parent.children.indexOf(mod);
     if (index !== -1) mod.parent.children.splice(index, 1);
   });
-
   client.levelCache = {};
   for (let i = 0; i < client.config.permLevels.length; i++) {
     const thisLevel = client.config.permLevels[i];
     client.levelCache[thisLevel.name] = thisLevel.level;
   }
-
   client.login(SHIRO_TOKEN);
 };
 
