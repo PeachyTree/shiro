8d9d020d80c87f3824687c412f1ea68ed23bae5c
PeachyTree <dragonminner867@outlook.com>
2021-08-11 21:49:15 +0200

    Remove the (useless) error event

--- events/error.js
+++
@@ -1,9 +0,0 @@
-module.exports = class {
-  constructor(client) {
-    this.client = client;
-  }
-
-  async run(error) {
-    this.client.logger.error(`Discord API Error: ${error.message}`);
-  }
-};
