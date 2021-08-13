bc7a1f3c8d4781f6cd5d1c41955a76767d792b7a
PeachyTree <dragonminner867@outlook.com>
2021-08-13 12:40:51 +0200

    Shiro 5.0.0!

---
+++ commands/core/changelog.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+const request = require('node-superfetch');
+const { shorten, embedURL } = require('../../util/Util');
+const { GITHUB_ACCESS_TOKEN, SHIRO_GITHUB_REPO_USERNAME, SHIRO_GITHUB_REPO_NAME } = process.env;
+
+module.exports = class ChangelogCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'changelog',
+			aliases: ['updates', 'commits'],
+			group: 'core',
+			memberName: 'changelog',
+			description: 'Responds with the bot\'s latest 10 commits.',
+			guarded: true
+		});
+	}
+
+	async run(msg) {
+		const { body } = await request
+			.get(`https://api.github.com/repos/${SHIRO_GITHUB_REPO_USERNAME}/${SHIRO_GITHUB_REPO_NAME}/commits`)
+			.set({ Authorization: `token ${GITHUB_ACCESS_TOKEN}` });
+		const commits = body.slice(0, 10);
+		const embed = new MessageEmbed()
+			.setTitle(`[${SHIRO_GITHUB_REPO_NAME}:master] Latest 10 commits`)
+			.setColor(0x7289DA)
+			.setURL(`https://github.com/${SHIRO_GITHUB_REPO_USERNAME}/${SHIRO_GITHUB_REPO_NAME}/commits/master`)
+			.setDescription(commits.map(commit => {
+				const hash = embedURL(`\`${commit.sha.slice(0, 7)}\``, commit.html_url, false);
+				return `${hash} ${shorten(commit.commit.message.split('\n')[0], 50)} - ${commit.author.login}`;
+			}).join('\n'));
+		return msg.embed(embed);
+	}
+};
---
+++ commands/core/donate.js
@@ -0,0 +1,21 @@
+const Command = require('../../structures/Command');
+const { stripIndents } = require('common-tags');
+
+module.exports = class DonateCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'donate',
+			group: 'core',
+			memberName: 'donate',
+			description: 'Responds with the bot\'s donation links.',
+			guarded: true
+		});
+	}
+
+	run(msg) {
+    return msg.say(stripIndents`
+    Contribute to development!
+    https://paypal.me/AzuraApple
+  `);
+	}
+};
