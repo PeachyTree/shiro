227828678313075ca3a13bf2496d0f47caba9083
PeachyTree <dragonminner867@outlook.com>
2021-08-13 12:19:24 +0200

    Remove vaportext command

--- commands/Text/zalgolize.js
+++ commands/Text/zalgolize.js
@@ -1,36 +1,33 @@
-const Command = require('../Command');
+const Command = require('../../structures/Command');
 const zalgo = require('../../assets/json/zalgo');
 
-class Zalgolize extends Command {
-  constructor(client) {
-    super(client, {
-      name: "zalgolize",
-      description: "Sends the same message that you had sent, but zalgolized.",
-      category: "Text",
-      usage: "zalgolize <Text>"
-    });
-  }
+module.exports = class ZalgolizeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'zalgolize',
+			aliases: ['zalgo'],
+			group: 'text',
+			memberName: 'zalgolize',
+			description: 'Sends the same message that you had sent, but zalgolized.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text do you want to zalgolize?',
+					type: 'string'
+				}
+			]
+		});
+	}
 
-  async run(message, args) { 
-    try {
-      if (!args.length) {
-        return message.reply("Command Usage: `zalgolize <Text>`")
+	run(msg, { text }) {
+    let result = '';
+    for (let i = 0; i < text.length; i++) {
+      result += text[i];
+      for (const chars of Object.values(zalgo)) {
+        let count = Math.floor(Math.random() * 5);
+        while (count--) result += chars[Math.floor(Math.random() * chars.length)];
       }
-
-      let result = '';
-      const text = args.join(' ');
-      for (let i = 0; i < text.length; i++) {
-        result += text[i];
-        for (const chars of Object.values(zalgo)) {
-          let count = Math.floor(Math.random() * 5);
-          while (count--) result += chars[Math.floor(Math.random() * chars.length)];
-        }
-      }
-      return message.channel.send(result);
-    } catch (err) {
-      return message.reply(`Oh no, an error occurred: \`${err.message}\`.`);
     }
-  };
-}
-
-module.exports = Zalgolize;
+    return msg.say(result);
+	}
+};
